9ced1243adc4c6d0498aada6d6cd374446e04ea5
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2021-09-26 00:42:24 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+
+namespace stackk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack st = new Stack();
+            st.Push(23);
+            st.Push("Hrishi");
+            st.Push("kkk");
+            st.Push(55);
+            st.Push("karan");
+            foreach(object tt in st)
+            {
+                Console.Write(tt+" ");
+            }
+            st.Pop();
+            Console.WriteLine("\nAfter removing an item\n");
+            foreach (object tt in st)
+            {
+                Console.Write(tt + " ");
+            }
+            Console.WriteLine("\nElements at top is: "+st.Peek());
+            Console.WriteLine(st.Count);
+        }
+    }
+}
